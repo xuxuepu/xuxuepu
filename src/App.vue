beaf140d3d4f4d5e59b6671908b7ae3d8b353595
许学普 <xuxuepu@scf365.com>
2016-08-25 01:55:27 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+	  <tabbar>
+		<tabbar-item>
+		  <img slot="icon" src="https://o84lhz5xo.qnssl.com/master/src/assets/demo/icon_nav_button.png">
+		  <span slot="label">首页</span>
+		</tabbar-item>
+		<tabbar-item show-dot>
+		  <img slot="icon" src="https://o84lhz5xo.qnssl.com/master/src/assets/demo/icon_nav_msg.png">
+		  <span slot="label">图片</span>
+		</tabbar-item>
+		<tabbar-item selected>
+		  <img slot="icon" src="https://o84lhz5xo.qnssl.com/master/src/assets/demo/icon_nav_article.png">
+		  <span slot="label">简介</span>
+		</tabbar-item>
+		<tabbar-item selected>
+		  <img slot="icon" src="https://o84lhz5xo.qnssl.com/master/src/assets/demo/icon_nav_article.png">
+		  <span slot="label">联系</span>
+		</tabbar-item>
+		<tabbar-item>
+		  <img slot="icon" src="https://o84lhz5xo.qnssl.com/master/src/assets/demo/icon_nav_cell.png">
+		  <span slot="label">关于</span>
+		</tabbar-item>
+	  </tabbar>
+	</div>
+</template>
+
+<script>
+import Group from 'vux/dist/components/group'
+import Cell from 'vux/dist/components/cell'
+import Tabbar from 'vux/dist/components/tabbar'
+import TabbarItem from 'vux/dist/components/tabbar-item'
+
+export default {
+  components: {
+    Group, Cell, Tabbar, TabbarItem
+  }
+}
+</script>
+
+<style>
+@import '~vux/dist/vux.css';
+</style>
---
+++ src/components/Test.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="test">
+    <h1>{{ msg }}</h1>
+	<a href="#" class="weui_btn weui_btn_primary">按钮</a>
+	<a href="#" class="weui_btn weui_btn_disabled weui_btn_primary">按钮</a>
+	<a href="#" class="weui_btn weui_btn_warn">确认</a>
+	<a href="#" class="weui_btn weui_btn_disabled weui_btn_warn">确认</a>
+	<a href="#" class="weui_btn weui_btn_default">按钮</a>
+	<a href="#" class="weui_btn weui_btn_disabled weui_btn_default">按钮</a>
+	<div class="button_sp_area">
+		<a href="#" class="weui_btn weui_btn_plain_default">按钮</a>
+		<a href="#" class="weui_btn weui_btn_plain_primary">按钮</a>
+
+		<a href="#" class="weui_btn weui_btn_mini weui_btn_primary">按钮</a>
+		<a href="#" class="weui_btn weui_btn_mini weui_btn_default">按钮</a>
+	</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      // note: changing this line won't causes changes
+      // with hot-reload because the reloaded component
+      // preserves its current state and we are modifying
+      // its initial state.
+      msg: '哎哟，这个更屌！'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
